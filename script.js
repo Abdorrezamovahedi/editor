a7c4d61810c01f7aaeb965f7f4e6b2e91a788423
Abdorrezamovahedi <abdorrezamovahedi12@gmail.com>
2026-06-01 00:44:19 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,38 @@
+/* ================= SCROLL ANIMATION ================= */
+
 const observer = new IntersectionObserver((entries)=>{
 entries.forEach(entry=>{
 if(entry.isIntersecting){
 entry.target.classList.add("show");
 }
 });
-},{
-threshold:0.15
-});
+},{threshold:0.15});
 
 document.querySelectorAll(".hidden").forEach(el=>{
 observer.observe(el);
 });
 
-let x=0,y=0;
+/* ================= MOUSE PARALLAX ================= */
+
+let mouseX = 0;
+let mouseY = 0;
+
+let currentX = 0;
+let currentY = 0;
 
 document.addEventListener("mousemove",(e)=>{
-x=e.clientX;
-y=e.clientY;
+mouseX = e.clientX;
+mouseY = e.clientY;
 });
 
 function animate(){
-document.body.style.backgroundPosition =
-`${x/40}px ${y/40}px`;
+
+currentX += (mouseX - currentX) * 0.05;
+currentY += (mouseY - currentY) * 0.05;
+
+document.body.style.transform =
+`translate(${currentX/250}px, ${currentY/250}px)`;
+
 requestAnimationFrame(animate);
 }
 
