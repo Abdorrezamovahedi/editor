9306fdcbc73d749e9aab374dac370960632ce73a
Abdorrezamovahedi <abdorrezamovahedi12@gmail.com>
2026-05-31 23:17:35 +0330

    Update script.js

--- script.js
+++ script.js
@@ -1,27 +1,28 @@
-const observer = new IntersectionObserver(entries=>{
-
+const observer = new IntersectionObserver((entries)=>{
 entries.forEach(entry=>{
-
 if(entry.isIntersecting){
-
 entry.target.classList.add("show");
-
 }
-
 });
-
+},{
+threshold:0.15
 });
 
 document.querySelectorAll(".hidden").forEach(el=>{
-
 observer.observe(el);
-
 });
+
+let x=0,y=0;
 
 document.addEventListener("mousemove",(e)=>{
-
-document.body.style.backgroundPosition =
-`${e.clientX/30}px ${e.clientY/30}px`;
-
+x=e.clientX;
+y=e.clientY;
 });
 
+function animate(){
+document.body.style.backgroundPosition =
+`${x/40}px ${y/40}px`;
+requestAnimationFrame(animate);
+}
+
+animate();
